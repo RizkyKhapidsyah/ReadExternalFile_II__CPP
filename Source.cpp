42c97dc9a6e8e1c834a8275eabe06407bcaec2b1
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-03-12 23:25:15 +0700

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <conio.h>
+#include <fstream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	//stream untuk membaca file
+	ifstream myfile;
+	char sv_text;
+
+	//membuka file yang telah ada
+	myfile.open("Text.txt");
+
+	cout << "---------------------------" << endl;
+	cout << "Membaca File Teks (Cara II)" << endl;
+	cout << "---------------------------" << endl;
+
+	//fail() -> untuk memeriksa suatu pada kesalahan operasi file
+	if (!myfile.fail())
+	{
+		cout << "Isi dari File -> ";
+		//ulang selama program belum mencapai akhir konten dari file
+		while (!myfile.eof())
+		{
+			myfile.get(sv_text);
+			cout << sv_text;
+		}
+		myfile.close(); //menutup file
+	}
+	else {
+		cout << "File tidak ditemukan" << endl;
+	}
+
+	_getche();
+	return EXIT_SUCCESS;
+}
